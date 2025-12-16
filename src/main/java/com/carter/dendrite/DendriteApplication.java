e59d8591514c7b8e5a2fc1d237ee340bc3c04b67
Carter <carter@tradev-group.com>
2025-12-16 13:40:34 +0800

    init

---
+++ src/main/java/com/carter/dendrite/DendriteApplication.java
@@ -0,0 +1,13 @@
+package com.carter.dendrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DendriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DendriteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carter/dendrite/DendriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carter.dendrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DendriteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
